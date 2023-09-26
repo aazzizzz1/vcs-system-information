f420704c4e187acef7b767ba115287fc254af759
aazzizzz1 <azizvbi33@gmail.com>
2023-09-26 16:18:17 +0700

    Revert "flowbite react"
    This reverts commit 430235f3ffdd54ddf94bfbf0a3f8ace14184207a.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,7 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
-    "./public/index.html",
-    "./node_modules/flowbite/**/*.js, jsx, ts, tsx}",
-    './node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+    "./node_modules/flowbite/**/*.js, jsx, ts, tsx}"
   ],
   theme: {
     extend: {},
