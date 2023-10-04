e8d495ba796d10f5fa52041cb00fce34e8b2104c
aazzizzz1 <azizvbi33@gmail.com>
2023-10-04 11:23:21 +0700

    refactoring auth and toast

--- src/Pages/Auth/SignIn.jsx
+++ src/Pages/Auth/SignIn.jsx
@@ -23,16 +23,16 @@ const SignIn = () => {
 
   return (
     <>
-      <section className="bg-gray-50 dark:bg-gray-900">
+      <section className="bg-gray-50 dark:bg-gray-900 p-30">
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-          {successMessage && <SuccessToast message={successMessage} />}
-          {errorMessage && <ErrorToast message={errorMessage} />}
           <a
             href="a"
             className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
           >
             <img className="w-40 h-30 mr-2" src={LogoLen} alt="Logo-Len" border="0" />
           </a>
+          {successMessage && <SuccessToast message={successMessage} />}
+          {errorMessage && <ErrorToast message={errorMessage} />}
           <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
